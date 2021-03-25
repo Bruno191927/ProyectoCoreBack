fc7fd41c7c93b8aedb237ef7d3e52d38cdaef909
Bruno <bruno.aguirre.p1997@gmail.com>
2021-03-24 19:27:45 -0500

    menu izquierdo

--- cursos-online-app/src/components/navegacion/bar/BarSesion.js
+++ cursos-online-app/src/components/navegacion/bar/BarSesion.js
@@ -1,7 +1,8 @@
-import { Avatar, Button, IconButton, makeStyles, Toolbar, Typography } from '@material-ui/core';
-import React from 'react';
+import { Avatar, Button, Drawer, IconButton, List, ListItem, ListItemText, makeStyles, Toolbar, Typography } from '@material-ui/core';
+import React,{useState} from 'react';
 import { useStateValue } from '../../../context/store';
 import FotoUsuarioTemp from '../../../logo.svg';
+import { MenuIzquierda } from './menuIzquierda';
 const useStyles = makeStyles((theme)=>({
     seccionDesktop :{
         display:"none",
@@ -21,6 +22,15 @@ const useStyles = makeStyles((theme)=>({
     avatarSize :{
         width:40,
         height:40
+    },
+    list:{
+        width:250
+    },
+    listItemText:{
+        fontSize: "14px",
+        fontWeight: 600,
+        paddingLeft : "15px",
+        color :"#212121"
     }
 }));
 
@@ -28,29 +38,54 @@ const BarSesion = () => {
 
     const classes = useStyles();
     const [{sesionUsuario}, dispatch ] = useStateValue();
+    const [abrirMenuIzquierda,setAbrirMenuIzquierda] = useState(false);
     
+    const cerrarMenuIzquierda = () => {
+        setAbrirMenuIzquierda(false);
+    }
+    const abrirMenuIzquierdaAction = () =>{
+        setAbrirMenuIzquierda(true);
+    }
+
     return (
-        <Toolbar>
-            <IconButton color="inherit">
-                <i className="material-icons">menu</i>
-            </IconButton>
-            <Typography variant="h6">Cursos Online</Typography>
-            <div className={classes.grow}></div>
-            <div className={classes.seccionDesktop}>
-                <Button color="inherit">
-                    Salir
-                </Button>
-                <Button color="inherit">
-                    {sesionUsuario ? sesionUsuario.usuario.nombreCompleto : ""}
-                </Button>
-                <Avatar src={FotoUsuarioTemp}></Avatar>
-            </div>
-            <div className={classes.seccionMobile}>
-                <IconButton color="inherit">
-                    <i className="material-icons">more_vert</i>
+        <React.Fragment>
+            <Drawer 
+                open={abrirMenuIzquierda} 
+                onClose={cerrarMenuIzquierda} 
+                anchor="left"
+            >
+                <div 
+                    className={classes.list} 
+                    onKeyDown={cerrarMenuIzquierda} 
+                    onClick={cerrarMenuIzquierda}
+                >
+                    <MenuIzquierda classes={classes} />
+                </div>
+            </Drawer>
+            <Toolbar>
+                <IconButton color="inherit" onClick={abrirMenuIzquierdaAction}>
+                    <i className="material-icons">menu</i>
                 </IconButton>
-            </div>
+
+                <Typography variant="h6">Cursos Online</Typography>
+                <div className={classes.grow}></div>
+                <div className={classes.seccionDesktop}>
+                    <Button color="inherit">
+                        Salir
+                    </Button>
+                    <Button color="inherit">
+                        {sesionUsuario ? sesionUsuario.usuario.nombreCompleto : ""}
+                    </Button>
+                    <Avatar src={FotoUsuarioTemp}></Avatar>
+                </div>
+                <div className={classes.seccionMobile}>
+                    <IconButton color="inherit">
+                        <i className="material-icons">more_vert</i>
+                    </IconButton>
+                </div>
         </Toolbar>
+        </React.Fragment>
+        
     );
 };
 
